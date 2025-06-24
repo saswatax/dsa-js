1a6fb4f31bb2fd461824e07a8111369ffa53475c
saswata <125185423+saswatax@users.noreply.github.com>
2025-06-24 18:24:19 +0530

    changes

---
+++ array/longest-even-odd.js
@@ -0,0 +1,56 @@
+/**
+ *
+ * @param {number[]} arr
+ * @returns {number}
+ */
+function longestEvenOdd(arr) {
+  let res = 1;
+  let cur = 1;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (
+      (arr[i] % 2 === 0 && arr[i - 1] % 2 !== 0) ||
+      (arr[i - 1] % 2 === 0 && arr[i] % 2 !== 0)
+    ) {
+      cur++;
+      res = Math.max(res, cur);
+    } else {
+      cur = 1;
+    }
+  }
+
+  return res;
+}
+
+/**
+ *
+ * @param {number[]} arr
+ * @returns {number}
+ */
+function longestEvenOdd_complex(arr) {
+  let res = 0;
+  let evenCount = 0;
+  let oddCount = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+
+    if (Math.abs(evenCount - oddCount) > 1) {
+      evenCount = 0;
+      oddCount = 0;
+      i--;
+    } else {
+      res = Math.max(res, evenCount + oddCount);
+    }
+  }
+
+  return res;
+}
+
+console.log(longestEvenOdd([10, 12, 14, 7, 8]));
+console.log(longestEvenOdd([7, 10, 13, 14]));
+console.log(longestEvenOdd([10, 12, 8, 4]));
---
+++ array/maximum-circular-subarray.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+function maximumCircularSubArray(arr) {
+  let res = arr[0];
+  let cur = arr[0];
+
+  for (let i = 1; i < 2 * arr.length - 1; i++) {
+    cur = Math.max(cur + arr[i % arr.length], arr[i % arr.length]);
+    res = Math.max(res, cur);
+  }
+
+  return res;
+}
+
+console.log(maximumCircularSubArray([5, -2, 3, 4]));
+console.log(maximumCircularSubArray([2, 3, -4]));
+console.log(maximumCircularSubArray([8, -4, 3, -5, 4]));
+console.log(maximumCircularSubArray([-3, 4, 6, -2]));
