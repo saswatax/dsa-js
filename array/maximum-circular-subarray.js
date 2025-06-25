0aaeaaefd3d501edec49c8250839b362c30003b3
saswata <125185423+saswatax@users.noreply.github.com>
2025-06-25 10:47:35 +0530

    changes

--- array/maximum-circular-subarray.js
+++ array/maximum-circular-subarray.js
@@ -3,13 +3,34 @@
  * @returns {number}
  */
 function maximumCircularSubArray(arr) {
+  const maxNormal = maximumSubArray(arr);
+
+  if (maxNormal < 0) {
+    return maxNormal;
+  }
+
+  let arrSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    arrSum += arr[i];
+    arr[i] = -arr[i];
+  }
+
+  const circularMax = arrSum + maximumSubArray(arr);
+
+  return Math.max(maxNormal, circularMax);
+}
+
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+function maximumSubArray(arr) {
   let res = arr[0];
   let cur = arr[0];
 
-  for (let i = 1; i < 2 * arr.length - 1; i++) {
-    cur = Math.max(cur + arr[i % arr.length], arr[i % arr.length]);
+  for (let i = 1; i < arr.length; i++) {
+    cur = Math.max(cur + arr[i], arr[i]);
     res = Math.max(res, cur);
-    console.log(arr[i % arr.length]);
   }
 
   return res;
